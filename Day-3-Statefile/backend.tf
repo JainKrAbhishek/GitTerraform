87ea3e42ad0c089f7904149d9e5b631d2d8ce2f2
Abhishek <abhishekadinath@gmail.com>
2025-02-26 22:40:10 +0530

    Day -4

--- Day-3-Statefile/backend.tf
+++ Day-3-Statefile/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "statefilestoragebucket"
+    bucket = "jainbucketdynmo"
     key    = "terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "terraform-state-lock-dynamo"
+    encrypt = true
   }
 }
