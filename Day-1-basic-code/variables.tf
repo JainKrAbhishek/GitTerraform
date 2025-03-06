4e9a2b0ac4b7f47f2aef3eac1c63f5c3e993012c
JainKrAbhishek <abhishekadinath@gmail.com>
2025-03-06 13:50:47 +0530

    Update variables.tf

--- Day-1-basic-code/variables.tf
+++ Day-1-basic-code/variables.tf
@@ -1,18 +1,18 @@
 variable "AMI-ID" {
     description = "AMI ID"
     type = string
-    default = ""
+    default = "ami-0d682f26195e9ec0f"
   
 }
 variable "INSTANCE-TYPE" {
         description = "Value of instance type"
         type = string
-        default = ""
+        default = "t2.micro""
 }
 
 variable "KEY-NAME" {
     description = "Key Name"
     type = string
-    default = ""
+    default = "NewKey""
   
 }
